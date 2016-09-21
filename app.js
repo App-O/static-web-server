3a3b0321890135492b34230787e9ed7afaaf4bf9
meg768 <magnus@egelberg.se>
2016-09-21 14:36:01 +0200

    -

---
+++ app.js
@@ -0,0 +1,26 @@
+var fs = require('fs');
+var Path = require('path');
+var express = require('express');
+var app = express();
+var mkpath = require('yow').mkpath;
+
+function setupStatics() {
+	var path = Path.join(__dirname, 'www');
+
+	mkpath(path);
+
+	fs.readdirSync(path).forEach(function(file) {
+		var fileName = Path.join(path, file);
+		var stats = fs.statSync(fileName);
+
+		if (stats.isDirectory()) {
+			app.use('/' + file, express.static(Path.join(path, file)));
+		};
+	});
+}
+
+setupStatics();
+
+app.listen(80, function () {
+  console.log('Listening on port 80.');
+});
