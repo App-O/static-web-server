1e2a682d66a935c0b4c3d716fbe5e2558d91f0c3
meg768 <magnus@egelberg.se>
2016-10-14 16:32:07 +0200

    -

--- start-webserver.js
+++ start-webserver.js
@@ -27,10 +27,11 @@ prefixLogs();
 
 if (cmd.log) {
 	var date = new Date();
-	var logFile = sprintf('%s/logs/%04d-%02d-%02d-%02d-%02d-%02d.log', __dirname, date.getFullYear(), date.getMonth() + 1, date.getDate(), date.getHours(), date.getMinutes(), date.getSeconds());
+	var path = sprintf('%s/logs', __dirname);
+	var name = sprintf('%04d-%02d-%02d-%02d-%02d-%02d.log', date.getFullYear(), date.getMonth() + 1, date.getDate(), date.getHours(), date.getMinutes(), date.getSeconds());
 
-	mkpath(logFile);
-	redirectLogs(logFile);
+	mkpath(path);
+	redirectLogs(Path.join(path, name));
 }
 
 app.use(express.static(cmd.root));
